043bcf0f03c711ffaa8bc3030e3f668e3c78536b
nigomezl <nigomezl@unal.edu.co>
2025-02-25 23:41:57 -0500

    Add files via upload

---
+++ sockets-master/src/socket/control.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package socket;
+
+/**
+ *
+ * @author locon
+ */
+public class control {
+    
+    public static void main(String[] args) {
+        
+        socket1 cliente = new socket1();
+        
+    }
+    
+}
